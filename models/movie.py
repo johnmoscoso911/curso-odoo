47a190aa2be4f7cb59130837bf4d23a6f0376059
John <johnmoscoso911@gmail.com>
2023-03-02 10:10:37 -0500

    compute

--- models/movie.py
+++ models/movie.py
@@ -20,9 +20,15 @@ class Movie(models.Model):
     name = fields.Char(required=True)
     year = fields.Selection(selection='_last_10_years', string='Release year', default=_default_year)
     duration = fields.Integer(string='Duration (min)')
+    duration_display = fields.Char(compute='_compute_duration_display', string='Duration')
     description = fields.Text()
     genre_ids = fields.Many2many('imdb.genre', 'rel_movie_genre', string='Genres')
 
     _sql_constraints = [
         ('check_duration', 'check(duration > 0)', _('Duration must be greater than 0'))
     ]
+
+    @api.depends('duration')
+    def _compute_duration_display(self):
+        for movie in self:
+            movie.duration_display = '%dh %dmin' % (3, 1)
