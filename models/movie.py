3963d2755a0d0f0e7c58c565d2fc48c72f4d137b
John <johnmoscoso911@gmail.com>
2023-03-02 10:00:50 -0500

    movie

--- models/movie.py
+++ models/movie.py
@@ -2,13 +2,27 @@
 
 from odoo import api, models, fields, _
 
+_CURRENT_YEAR = fields.Datetime.today().year
+
+
+def _last_10_years():
+    return [(x, x) for x in range(_CURRENT_YEAR + 1, _CURRENT_YEAR - 10, -1)]
+
 
 class Movie(models.Model):
     _name = 'imdb.movie'
     _description = 'Movie'
 
-    name = fields.Char(required=True, translate=True, help='The description')
+    @api.model
+    def _default_year(self):
+        return _CURRENT_YEAR
+
+    name = fields.Char(required=True)
+    year = fields.Selection(selection='_last_10_years', string='Release year', default=_default_year)
+    duration = fields.Integer(string='Duration (min)')
+    description = fields.Text()
+    genre_ids = fields.Many2many('imdb.genre', 'rel_movie_genre', string='Genres')
 
     _sql_constraints = [
-        ('unique_name', 'unique(name)', _('Movie must be unique'))
+        ('check_duration', 'check(duration > 0)', _('Duration must be greater than 0'))
     ]
